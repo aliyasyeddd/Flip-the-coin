7a364f1f6ed9078c2d1c5e75dcf2727ecb6dbe8f
aliyasyeddd <mahaboobaliya02@gmail.com>
2025-03-14 11:33:08 +0530

    adding coin functionality and css changes

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const btnFlip = document.querySelector("#btn");
 const imgSrc = document.querySelector(".coin-img");
 const coinResult = document.querySelector("#result");
 
-btnFlip.addEventListener("click", () => {
+function flipCoin() {
   imgSrc.src = imgSrc.src.includes("heads") ? "./resources/tails.svg" : "./resources/heads.svg";
   coinResult.textContent = imgSrc.src.includes("heads") ? "Heads" : "Tails";
-});
+}
+
+btnFlip.addEventListener("click",flipCoin);
+imgSrc.addEventListener("click",flipCoin);
 
 
 
